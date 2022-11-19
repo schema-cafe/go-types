3419526bb66b5f071ba8c9939589f050842df847
Michael Rybka <merybka@gmail.com>
2022-11-19 17:42:27 -0500

    wip

--- ui/element.go
+++ ui/element.go
@@ -2,6 +2,6 @@ package ui
 
 type Element struct {
 	Type     string            `json:"type"`
-	Attrs    map[string]string `json:"Attrs"`
+	Attrs    map[string]string `json:"attrs"`
 	Children []Element         `json:"children"`
 }
