af28e83afe3585bd9062db9ecfdc252825149744
Michael Rybka <merybka@gmail.com>
2022-11-21 21:14:37 -0500

    Add password hash to ui user

--- ui/user.go
+++ ui/user.go
@@ -1,7 +1,8 @@
 package ui
 
 type User struct {
-	ID          string   `json:"id"`
-	Name        []string `json:"name"`
-	GithubToken string   `json:"githubToken"`
+	ID           string   `json:"id"`
+	Name         []string `json:"name"`
+	PasswordHash string   `json:"passwordHash"`
+	GithubToken  string   `json:"githubToken"`
 }
